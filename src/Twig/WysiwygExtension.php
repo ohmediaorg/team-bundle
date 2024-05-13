2f112fb8c67cd23c8eb984e59837fd32e5d4b19e
Justin Hoffman <justin@ohmedia.ca>
2024-05-13 15:27:39 -0600

    implement twig function + schema

--- src/Twig/WysiwygExtension.php
+++ src/Twig/WysiwygExtension.php
@@ -2,9 +2,13 @@
 
 namespace OHMedia\TeamBundle\Twig;
 
+use OHMedia\FileBundle\Service\FileManager;
+use OHMedia\SettingsBundle\Service\Settings;
+use OHMedia\TeamBundle\Entity\TeamMember;
 use OHMedia\TeamBundle\Repository\TeamRepository;
 use OHMedia\WysiwygBundle\Service\Wysiwyg;
 use OHMedia\WysiwygBundle\Twig\AbstractWysiwygExtension;
+use Symfony\Component\HttpFoundation\UrlHelper;
 use Twig\Environment;
 use Twig\TwigFunction;
 
@@ -13,7 +17,10 @@ class WysiwygExtension extends AbstractWysiwygExtension
     private array $teams = [];
 
     public function __construct(
+        private FileManager $fileManager,
+        private Settings $settings,
         private TeamRepository $teamRepository,
+        private UrlHelper $urlHelper,
         private Wysiwyg $wysiwyg,
     ) {
     }
@@ -49,20 +56,90 @@ class WysiwygExtension extends AbstractWysiwygExtension
             return '';
         }
 
-        foreach ($members as $member) {
-            $schema = [
-                '@context' => 'https://schema.org',
-                '@type' => 'Person',
-                // ...
-          ];
-        }
+        $schemaOrganizationName = $this->settings->get('schema_organization_name');
 
         $rendered = $twig->render('@OHMediaTeam/team.html.twig', [
             'team' => $team,
         ]);
 
-        $rendered .= '<script type="application/ld+json">'.json_encode($schema).'</script>';
+        foreach ($members as $member) {
+            $schema = $this->getTeamMemberSchema($member);
+
+            if ($schemaOrganizationName) {
+                $schema['worksFor'] = [
+                    '@type' => 'Organization',
+                    'name' => $schemaOrganizationName,
+                ];
+            }
+
+            $rendered .= '<script type="application/ld+json">'.json_encode($schema).'</script>';
+        }
 
         return $rendered;
+    }
+
+    private function getTeamMemberSchema(TeamMember $teamMember): array
+    {
+        $schema = [
+            '@context' => 'https://schema.org/',
+            '@type' => 'Person',
+            'name' => (string) $teamMember,
+        ];
+
+        if ($honorific = $teamMember->getHonorific()) {
+            $schema['honorificPrefix'] = $honorific;
+        }
+
+        if ($designation = $teamMember->getDesignation()) {
+            $schema['honorificSuffix'] = $designation;
+        }
+
+        if ($title = $teamMember->getTitle()) {
+            $schema['jobTitle'] = $title;
+        }
+
+        if ($bio = $teamMember->getBio()) {
+            $schema['description'] = $bio;
+        }
+
+        $image = $teamMember->getImage();
+
+        if ($image && $image->getPath()) {
+            $webPath = $this->fileManager->getWebPath($image);
+
+            $schema['image'] = $this->urlHelper->getAbsoluteUrl($webPath);
+        }
+
+        if ($email = $teamMember->getEmail()) {
+            $schema['email'] = $email;
+        }
+
+        if ($phone = $teamMember->getPhone()) {
+            $schema['telephone'] = $phone;
+        }
+
+        $socials = [];
+
+        if ($facebook = $teamMember->getFacebook()) {
+            $socials[] = $facebook;
+        }
+
+        if ($twitter = $teamMember->getTwitter()) {
+            $socials[] = $twitter;
+        }
+
+        if ($instagram = $teamMember->getInstagram()) {
+            $socials[] = $instagram;
+        }
+
+        if ($linkedIn = $teamMember->getLinkedIn()) {
+            $socials[] = $linkedIn;
+        }
+
+        if ($socials) {
+            $schema['sameAs'] = $socials;
+        }
+
+        return $schema;
     }
 }
